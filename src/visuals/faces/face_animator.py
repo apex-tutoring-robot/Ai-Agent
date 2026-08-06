990f350a52a806e54a8d9f0ba9c75532ebe89bc2
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2026-08-06 10:56:19 -0700

    Update face_animator.py

--- src/visuals/faces/face_animator.py
+++ src/visuals/faces/face_animator.py
@@ -126,7 +126,7 @@ class FaceAnimator:
         if self.blink > 0.7:
             frame = self.faces["blinking"].copy()
 
-        return cv2.resize(frame, (1024, 600))
+        return cv2.resize(frame, (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT))
 
     # --------------------------------------------------
     # Render Loop
